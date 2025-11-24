045fb337ad5d1c67a6ad0cbd8c8cb409be46e4a2
Maryam Abu Hishmeh <mabohishma@gmail.com>
2025-11-24 16:02:44 +0300

    Update generator.cc

--- GEANT4/Training First Project/src/generator.cc
+++ GEANT4/Training First Project/src/generator.cc
@@ -4,13 +4,7 @@
 MyPrimaryGenerator::MyPrimaryGenerator()
 {
     fParticleGun = new G4ParticleGun(1);
-}
-MyPrimaryGenerator::~MyPrimaryGenerator()
-{
-    delete fParticleGun;
-}
-void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
-{
+    //---------------------------------------------------------------------------------------
     G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
     G4String particleName="proton";
     G4ParticleDefinition *particle = particleTable->FindParticle("proton");
@@ -22,6 +16,26 @@ void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
     fParticleGun->SetParticleMomentumDirection(mom);
     fParticleGun->SetParticleMomentum(100.*GeV);
     fParticleGun->SetParticleDefinition(particle);
+//--------------------------------------------------------------------------------------//we added these here!
+}
+MyPrimaryGenerator::~MyPrimaryGenerator()
+{
+    delete fParticleGun;
+}
+void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
+{
+     /* G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
+    G4String particleName="proton";
+    G4ParticleDefinition *particle = particleTable->FindParticle("proton");
+
+    G4ThreeVector pos(0. ,0. ,0. );
+    G4ThreeVector mom(0. ,0. ,1. );
+
+    fParticleGun->SetParticlePosition(pos);
+    fParticleGun->SetParticleMomentumDirection(mom);
+    fParticleGun->SetParticleMomentum(100.*GeV);
+    fParticleGun->SetParticleDefinition(particle);*///---------->we have to delete all of these if we want to change the momentum and add it to the constructor
+
 
     fParticleGun->GeneratePrimaryVertex(anEvent);
 }
