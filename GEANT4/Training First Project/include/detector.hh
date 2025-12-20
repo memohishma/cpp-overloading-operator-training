dc81758edee51ac3513ebaa010a64117c48db19e
Maryam Abu Hishmeh <mabohishma@gmail.com>
2025-12-21 01:00:11 +0300

    Update detector.hh

--- GEANT4/Training First Project/include/detector.hh
+++ GEANT4/Training First Project/include/detector.hh
@@ -1,7 +1,12 @@
+  GNU nano 7.2                                                                      include/detector.hh                                                                                
 #ifndef DETECTOR_HH
 #define DETECTOR_HH
 
+
 #include "G4VSensitiveDetector.hh"
+//#include "g4root.hh"
+#include "G4RunManager.hh"
+#include "G4SystemOfUnits.hh"
 
 class MySensitiveDetector : public G4VSensitiveDetector
 {
@@ -12,5 +17,9 @@ class MySensitiveDetector : public G4VSensitiveDetector
   private:
   virtual G4bool ProcessHits(G4Step*,G4TouchableHistory*);
 
+   G4PhysicsFreeVector *quEff;
+
 };
 #endif
+
+
