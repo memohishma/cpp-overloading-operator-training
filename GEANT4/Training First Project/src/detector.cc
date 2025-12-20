a552958e1d73c0d24658877134e7a3b3bbc3231d
Maryam Abu Hishmeh <mabohishma@gmail.com>
2025-12-21 00:58:50 +0300

    Update detector.cc

--- GEANT4/Training First Project/src/detector.cc
+++ GEANT4/Training First Project/src/detector.cc
@@ -1,9 +1,36 @@
+                                                                                                                                                                                                                                                                                                            src/detector.cc                                                                                                                                                                                                                                                                                                                        
 #include "detector.hh"
+#include "G4AnalysisManager.hh"
 
 MySensitiveDetector::MySensitiveDetector(G4String name):G4VSensitiveDetector(name)
-{}
+{
+   quEff = new G4PhysicsFreeVector();
+
+   std::ifstream datafile;
+   datafile.open("eff.dat");
+
+  while(1)
+  {
+     G4double wlen, queff;
+
+     datafile >> wlen >> queff;
+
+    if(datafile.eof())
+       break;
+
+      G4cout<< wlen << " " <<queff<<G4endl;
+
+      quEff -> InsertValues(wlen, queff/100.);
+  }
+    datafile.close();
+
+   //quEff->SetSpline(false);
+
+}
 MySensitiveDetector::~MySensitiveDetector()
-{}
+{
+    delete quEff;
+}
  G4bool MySensitiveDetector::ProcessHits(G4Step *aStep, G4TouchableHistory *ROhist)
 {
    G4Track *track = aStep->GetTrack();
@@ -14,6 +41,9 @@ MySensitiveDetector::~MySensitiveDetector()
    G4StepPoint *postStepPoint = aStep->GetPostStepPoint();
 
    G4ThreeVector posPhoton = preStepPoint->GetPosition();
+   G4ThreeVector momPhoton = preStepPoint->GetMomentum();
+
+    G4double wlen = ((1.239841939*eV)*1E+03/momPhoton.mag());
 
   // G4cout<<"Photon Position: "<<posPhoton<<G4endl;
 
@@ -22,6 +52,39 @@ MySensitiveDetector::~MySensitiveDetector()
     G4int copyNo = touchable->GetCopyNumber();
 
    // G4cout<<"Copy Number: "<<copyNo<<G4endl;
+
+    G4VPhysicalVolume *physVol = touchable->GetVolume();
+    G4ThreeVector posDetector = physVol->GetTranslation();
+
+    #ifdef G4MULTITHREADED
+
+    G4cout<<"Detector Position: "<<posDetector<<G4endl; 
+
+    #endif
+
+    G4int evt = G4RunManager::GetRunManager()->GetCurrentEvent()->GetEventID();
+
+    G4AnalysisManager *man = G4AnalysisManager::Instance();
+
+    man->FillNtupleIColumn(0, 0, evt);
+    man->FillNtupleDColumn(0,1,posPhoton[0]);
+    man->FillNtupleDColumn(0,2,posPhoton[1]);
+    man->FillNtupleDColumn(0,3,posPhoton[2]);
+    man->FillNtupleDColumn(0,4,wlen);
+
+    if(G4UniformRand() < quEff -> Value(wlen))
+   {
+    man->FillNtupleIColumn(1, 0, evt);
+    man->FillNtupleDColumn(1,1,posDetector[0]);
+    man->FillNtupleDColumn(1,2,posDetector[1]);
+    man->FillNtupleDColumn(1,3,posDetector[2]);
+    man->AddNtupleRow(1);
+    }
+ 
+   return true;
+}
+
+
 
     G4VPhysicalVolume *physVol = touchable->GetVolume();
     G4ThreeVector posDetector = physVol->GetTranslation();
