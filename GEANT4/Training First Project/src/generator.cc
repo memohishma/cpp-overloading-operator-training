23897f836a1d2cd77df821f44e9494d20c9d5338
Maryam Abu Hishmeh <mabohishma@gmail.com>
2025-12-21 01:01:52 +0300

    Update generator.cc

--- GEANT4/Training First Project/src/generator.cc
+++ GEANT4/Training First Project/src/generator.cc
@@ -1,20 +1,21 @@
 #include "generator.hh"
+#include "G4Geantino.hh"
+#include "G4IonTable.hh"
 
 
 MyPrimaryGenerator::MyPrimaryGenerator()
 {
     fParticleGun = new G4ParticleGun(1);
-    //---------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------
     G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
-    G4String particleName="proton";
-    G4ParticleDefinition *particle = particleTable->FindParticle("proton");
+    G4ParticleDefinition *particle = particleTable->FindParticle("geantino");
 
     G4ThreeVector pos(0. ,0. ,0. );
     G4ThreeVector mom(0. ,0. ,1. );
 
     fParticleGun->SetParticlePosition(pos);
     fParticleGun->SetParticleMomentumDirection(mom);
-    fParticleGun->SetParticleMomentum(100.*GeV);
+    fParticleGun->SetParticleMomentum(0.*GeV);
     fParticleGun->SetParticleDefinition(particle);
 //--------------------------------------------------------------------------------------//we added these here!
 }
@@ -24,18 +25,22 @@ MyPrimaryGenerator::~MyPrimaryGenerator()
 }
 void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
 {
-     /* G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
-    G4String particleName="proton";
-    G4ParticleDefinition *particle = particleTable->FindParticle("proton");
+    G4ParticleDefinition *particle = fParticleGun->GetParticleDefinition();
 
-    G4ThreeVector pos(0. ,0. ,0. );
-    G4ThreeVector mom(0. ,0. ,1. );
+    if(particle == G4Geantino::Geantino())
+      {
+       G4int Z=27;
+       G4int A=60;
 
-    fParticleGun->SetParticlePosition(pos);
-    fParticleGun->SetParticleMomentumDirection(mom);
-    fParticleGun->SetParticleMomentum(100.*GeV);
-    fParticleGun->SetParticleDefinition(particle);*///---------->we have to delete all of these if we want to change the momentum and add it to the constructor
+       G4double charge = 0.*eplus;
+       G4double energy = 0.*keV;
 
+       G4ParticleDefinition *ion = G4IonTable::GetIonTable()->GetIon(Z, A, energy);
 
-    fParticleGun->GeneratePrimaryVertex(anEvent);
+       fParticleGun->SetParticleDefinition(ion);
+       fParticleGun->SetParticleCharge(charge);
+
+     }
+     fParticleGun->GeneratePrimaryVertex(anEvent);
 }
+
