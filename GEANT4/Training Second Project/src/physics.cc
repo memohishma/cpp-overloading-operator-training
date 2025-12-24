65bb9d2d86dfa309df7c5c0e0d7725ed12ad9b00
Maryam Abu Hishmeh <mabohishma@gmail.com>
2025-12-24 06:02:34 +0300

    Create physics.cc

---
+++ GEANT4/Training Second Project/src/physics.cc
@@ -0,0 +1,15 @@
+#include "physics.hh"
+
+MyPhysicsList::MyPhysicsList()
+{
+     RegisterPhysics(new G4EmStandardPhysics());
+     RegisterPhysics(new G4DecayPhysics());
+     RegisterPhysics(new G4RadioactiveDecayPhysics());
+     RegisterPhysics(new G4HadronElasticPhysicsHP());
+     RegisterPhysics(new G4HadronPhysicsQGSP_BIC_HP());
+     RegisterPhysics(new G4NeutronTrackingCut());
+
+}
+MyPhysicsList::~MyPhysicsList()
+{}
+
