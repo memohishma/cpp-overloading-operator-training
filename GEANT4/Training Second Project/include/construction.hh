8b5e298afa44ad7e403a0efd408f0078de984af1
Maryam Abu Hishmeh <mabohishma@gmail.com>
2025-12-24 05:56:42 +0300

    Create construction.hh

---
+++ GEANT4/Training Second Project/include/construction.hh
@@ -0,0 +1,26 @@
+#ifndef CONSTRUCTION_HH
+#define CONSTRUCTION_HH
+
+#include "G4NistManager.hh"
+#include "G4Material.hh"
+#include "G4Box.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"
+#include "G4VPhysicalVolume.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4VUserDetectorConstruction.hh"
+#include "globals.hh"
+
+class MyDetector : public G4VUserDetectorConstruction
+{
+public:
+
+    MyDetector();
+  virtual ~MyDetector();
+
+   virtual G4VPhysicalVolume *Construct();
+
+
+};
+#endif
+
