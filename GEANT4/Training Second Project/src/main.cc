d2763414e06e3e2339995dc72c01dd061bc9c56a
Maryam Abu Hishmeh <mabohishma@gmail.com>
2025-12-24 06:04:29 +0300

    Create main.cc

---
+++ GEANT4/Training Second Project/src/main.cc
@@ -0,0 +1,40 @@
+#include<iostream>
+
+#include "G4RunManager.hh"
+#include "G4VisExecutive.hh"
+#include "G4UIExecutive.hh"
+#include "G4VisManager.hh"
+#include "G4UImanager.hh"
+
+#include "construction.hh"
+#include "physics.hh"
+#include "action.hh"
+#include "generator.hh"
+
+int main(int argc,char** argv)
+{
+
+    G4RunManager *runManager = new G4RunManager();
+
+    runManager->SetUserInitialization(new MyDetector());
+    runManager->SetUserInitialization(new MyPhysicsList());
+    runManager->SetUserInitialization(new MyAction());
+
+    runManager->Initialize();
+
+    G4UIExecutive *ui = new G4UIExecutive(argc,argv);
+
+    G4VisManager *visManager = new G4VisExecutive();
+    visManager->Initialize();
+
+    G4UImanager *UImanager =  G4UImanager::GetUIpointer();
+
+    UImanager->ApplyCommand("/vis/open OGL");
+    UImanager->ApplyCommand("/vis/viewer/setpointVector 1 1 1");
+    UImanager->ApplyCommand("/vis/drawVolume");
+    UImanager->ApplyCommand("/vis/viewer/set/autoRefresh");
+    UImanager->ApplyCommand("/vis/scene/add/trajectories smooth");
+    ui->SessionStart();
+
+   return 0;
+}
