413d26e898218c335456434eba92a5a0ac13465f
Maryam Abu Hishmeh <mabohishma@gmail.com>
2026-04-02 00:38:01 +0300

    Update generator.cpp

--- GEANT4/Lithium Target/include/generator.cpp
+++ GEANT4/Lithium Target/include/generator.cpp
@@ -1,1 +1,23 @@
+ifndef GENERATOR_HH
+#define GENERATOR_HH
 
+#include "G4VUserPrimaryGeneratorAction.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4ParticleTable.hh"
+#include "G4ParticleGun.hh"
+#include "G4ThreeVector.hh"
+
+class MyGenerator : public G4VUserPrimaryGeneratorAction
+{
+        public:
+
+        MyGenerator();
+         ~MyGenerator();
+
+        virtual void GeneratePrimaries(G4Event* anEvent) override;
+
+        private:
+        G4ParticleGun *fParticleGun;
+
+};
+#endif
