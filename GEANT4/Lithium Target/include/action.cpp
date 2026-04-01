e60dc0b2ba623bc8472b6302981e026ee7d93f1f
Maryam Abu Hishmeh <mabohishma@gmail.com>
2026-04-02 00:36:32 +0300

    Update and rename action to action.cpp

---
+++ GEANT4/Lithium Target/include/action.cpp
@@ -0,0 +1,21 @@
+
+#ifndef ACTION_HH
+#define ACTION_HH
+
+#include "G4VUserActionInitialization.hh"
+#include "generator.hh"
+
+
+class MyAction : public G4VUserActionInitialization
+{
+   public:
+     MyAction();
+     ~MyAction();
+
+
+    virtual void Build() const override;
+    virtual void BuildForMaster() const override;
+
+
+};
+#endif
