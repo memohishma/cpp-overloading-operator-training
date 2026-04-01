dd78d5486f13df4435487dc8ae1060793e6136e5
Maryam Abu Hishmeh <mabohishma@gmail.com>
2026-04-02 00:42:13 +0300

    Create action.cpp

---
+++ GEANT4/Lithium Target/src/action.cpp
@@ -0,0 +1,20 @@
+include "action.hh"
+#include "generator.hh"
+
+MyAction::MyAction()
+{}
+MyAction::~MyAction()
+{}
+
+void MyAction::Build() const
+{
+
+        MyGenerator *generator = new MyGenerator();
+        SetUserAction(generator);
+
+}
+void MyAction::BuildForMaster() const
+{
+
+
+}
