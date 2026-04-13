0b30ef2419eb6d57b643487992a585bfde45fe93
Maryam Abu Hishmeh <mabohishma@gmail.com>
2026-04-14 02:13:05 +0300

    Update physics.cpp

--- GEANT4/Lithium Target/src/physics.cpp
+++ GEANT4/Lithium Target/src/physics.cpp
@@ -1,16 +1,21 @@
-  GNU nano 7.2                            physics.cc                                      
-#include "physics.hh"
+ #include "physics.hh"
 
 MyPhysicsList::MyPhysicsList()
 {
-        RegisterPhysics(new G4EmStandardPhysics());
+
+        /*RegisterPhysics(new G4EmStandardPhysics());
         RegisterPhysics(new G4DecayPhysics());
         RegisterPhysics(new G4HadronElasticPhysicsHP());
         RegisterPhysics(new G4HadronPhysicsQGSP_BIC_HP());
         RegisterPhysics(new G4StoppingPhysics());
         RegisterPhysics(new G4IonPhysics());
-        RegisterPhysics(new G4NeutronTrackingCut());
+        RegisterPhysics(new G4NeutronTrackingCut());*/
+
 
 }
 MyPhysicsList::~MyPhysicsList()
 {}
+
+
+
+
