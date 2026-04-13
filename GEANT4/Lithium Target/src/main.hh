1354d2ea665a989b53c51806625bbbfe4091f267
Maryam Abu Hishmeh <mabohishma@gmail.com>
2026-04-14 02:13:58 +0300

    Update main.hh

--- GEANT4/Lithium Target/src/main.hh
+++ GEANT4/Lithium Target/src/main.hh
@@ -1,3 +1,4 @@
+  GNU nano 7.2                                                      src/main.cc                                                                
 #include <iostream>
 
 #include "G4RunManager.hh"
@@ -11,29 +12,45 @@
 #include "physics.hh"
 #include "generator.hh"
 
+#include "G4VModularPhysicsList.hh"
+#include "G4PhysListFactory.hh"
+
 int main(int argc, char** argv)
 {
    G4RunManager *runManager = new G4RunManager();
 
    runManager->SetUserInitialization(new MyDetector());
-   runManager->SetUserInitialization(new MyPhysicsList());
-   runManager->SetUserInitialization(new MyAction());
+//   runManager->SetUserInitialization(new MyPhysicsList());
+G4PhysListFactory factory;
+G4VModularPhysicsList* physics = factory.GetReferencePhysList("QGSP_BIC_HP");
+runManager->SetUserInitialization(physics);
+
+    runManager->SetUserInitialization(new MyAction());
 
    runManager->Initialize();
 
-   G4UIExecutive *ui = new G4UIExecutive(argc, argv);
+   G4UIExecutive *ui = 0;
+
+   if(argc == 1)
+   {
+   ui = new G4UIExecutive(argc, argv);
+   }
 
    G4VisManager *visManager = new G4VisExecutive();
    visManager->Initialize();
-
    G4UImanager *UImanager = G4UImanager::GetUIpointer();
 
-    UImanager->ApplyCommand("/vis/open OGL");
-    UImanager->ApplyCommand("/vis/viewer/setpointVector 1 1 1");
-    UImanager->ApplyCommand("/vis/drawVolume");
-    UImanager->ApplyCommand("/vis/viewer/set/autoRefresh");
-    UImanager->ApplyCommand("/vis/scene/add/trajectories smooth");
+   if(ui)
+   {
+    UImanager->ApplyCommand("/control/execute vis.mac");
     ui->SessionStart();
-
+    }
+    else
+    {
+        G4String command = "/control/execute";
+        G4String fileName = argv[1];
+        UImanager->ApplyCommand(command+fileName);
+    }
    return 0;
 }
+
