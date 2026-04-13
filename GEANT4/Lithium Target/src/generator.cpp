78a4bbcb8b6df34006cd47f0b21214a345651db9
Maryam Abu Hishmeh <mabohishma@gmail.com>
2026-04-14 02:12:26 +0300

    Update generator.cpp

--- GEANT4/Lithium Target/src/generator.cpp
+++ GEANT4/Lithium Target/src/generator.cpp
@@ -4,7 +4,17 @@ MyGenerator::MyGenerator()
 {
         fParticleGun = new G4ParticleGun(1);
 
+        G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
+        G4String particleName="proton";
+        G4ParticleDefinition *particle = particleTable->FindParticle("proton>
 
+        G4ThreeVector pos(0.,0.,-1.*mm);
+        G4ThreeVector mom(0.,0.,1.);
+
+        fParticleGun->SetParticlePosition(pos);
+        fParticleGun->SetParticleMomentumDirection(mom);
+        fParticleGun->SetParticleEnergy(3.0*MeV);
+        fParticleGun->SetParticleDefinition(particle);
 
 
 }
@@ -14,19 +24,10 @@ MyGenerator::~MyGenerator()
 }
 void MyGenerator::GeneratePrimaries(G4Event *anEvent)
 {
-        G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
-        G4String particleName="proton";
-        G4ParticleDefinition *particle = particleTable->FindParticle("proton");
-      
-        G4ThreeVector pos(0.,0.,-1.*mm);
-        G4ThreeVector mom(0.,0.,1.);
-
-        fParticleGun->SetParticlePosition(pos);
-        fParticleGun->SetParticleMomentumDirection(mom);
-        fParticleGun->SetParticleEnergy(2.2*MeV);
-        fParticleGun->SetParticleDefinition(particle);
 
         fParticleGun->GeneratePrimaryVertex(anEvent);
 
 }
+
+
 
