b53ea239362942b048b8cc84a98280b0646f9a25
Maryam Abu Hishmeh <mabohishma@gmail.com>
2026-04-14 02:07:55 +0300

    Update physics.cpp

--- GEANT4/Lithium Target/include/physics.cpp
+++ GEANT4/Lithium Target/include/physics.cpp
@@ -1,7 +1,7 @@
-#ifndef PHYSICS_HH
+ifndef PHYSICS_HH
 #define PHYSICS_HH
 
-#include "G4VModularPhysicsList.hh"
+/*#include "G4VModularPhysicsList.hh"
 #include "G4EmStandardPhysics.hh"
 #include "G4DecayPhysics.hh"
 #include "G4HadronElasticPhysicsHP.hh"
@@ -9,14 +9,17 @@
 #include "G4HadronPhysicsQGSP_BIC_HP.hh"
 #include "G4StoppingPhysics.hh"
 #include "G4IonPhysics.hh"
-#include "G4NeutronTrackingCut.hh"
-
-class MyPhysicsList : public G4VModularPhysicsList
+#include "G4NeutronTrackingCut.hh"*/
+#include "Shielding.hh"
+class MyPhysicsList : public Shielding
 {
         public:
 
         MyPhysicsList();
          ~MyPhysicsList();
 
+
 };
 #endif
+
+
