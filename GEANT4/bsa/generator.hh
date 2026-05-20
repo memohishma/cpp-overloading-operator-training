53e2f1cedea68b1362ee0f999fc2f50693ff3235
Maryam Abu Hishmeh <mabohishma@gmail.com>
2026-05-20 13:33:25 +0300

    Create generator.hh

---
+++ GEANT4/bsa/generator.hh
@@ -0,0 +1,26 @@
+#ifndef GENERATOR_HH
+#define GENERATOR_HH
+
+#include "G4VUserPrimaryGeneratorAction.hh"
+#include "G4ParticleGun.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4ParticleTable.hh"
+#include "Randomize.hh"
+#include <CLHEP/Random/Randomize.h>
+#include <cmath>
+
+//create a new Primary Generator class, inheriting from GEANT4's existing Primary Generator class
+class MyPrimaryGenerator : public G4VUserPrimaryGeneratorAction
+{
+    public:
+        MyPrimaryGenerator(); //constructor
+        ~MyPrimaryGenerator(); //destructor
+
+        virtual void GeneratePrimaries(G4Event*); //function to generate primary particles for each event
+
+    private:
+        G4ParticleGun *fParticleGun; //attribute to hold the pointer to the Particle Gun 
+        // the particle Gun is an object that describes how the simulation should "shoot" protons/any starting particle
+};
+
+#endif
