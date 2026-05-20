c0ae146d530b7d8e41cc8ed9368c06b0650410de
Maryam Abu Hishmeh <mabohishma@gmail.com>
2026-05-20 13:28:16 +0300

    Create action.cc

---
+++ GEANT4/bsa/action.cc
@@ -0,0 +1,31 @@
+#include "action.hh"
+
+MyActionInitialization::MyActionInitialization()
+{} //constructor (no additional action is required => empty)
+
+MyActionInitialization::~MyActionInitialization()
+{} //destructor (no additional action is required => empty)
+
+void MyActionInitialization::BuildForMaster() const
+{
+    //=========================================================================================================================
+    //this function initilizes all user actions taken in the master thread
+    //=========================================================================================================================
+
+    MyRunAction *runAction = new MyRunAction(); //We only need a run action, to control what happens before/after each run
+    SetUserAction(runAction);
+}
+
+void MyActionInitialization::Build() const
+{
+    //=========================================================================================================================
+    //this function initilizes all user actions taken in the worker threads
+    //=========================================================================================================================
+
+    MyPrimaryGenerator *generator = new MyPrimaryGenerator(); //initilize the Primary Generator here!
+    SetUserAction(generator);
+    // generating primary particles only happens in the worker threads
+
+    MyRunAction *runAction = new MyRunAction(); //run action to control what happens before/after each run for worker threads
+    SetUserAction(runAction);
+}
