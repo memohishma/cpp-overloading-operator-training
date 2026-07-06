df2df440868e9586be6ac766005644b4e8674304
Maryam Abu Hishmeh <mabohishma@gmail.com>
2026-07-06 13:30:05 +0300

    Create NeutronHPphysics.hh

---
+++ GEANT4/Beam_Shaping_Assembly/NeutronHPphysics.hh
@@ -0,0 +1,25 @@
+#ifndef NeutronHPphysics_h
+#define NeutronHPphysics_h 1
+
+#include "G4VPhysicsConstructor.hh"
+#include "globals.hh"
+
+class G4GenericMessenger;
+
+class NeutronHPphysics : public G4VPhysicsConstructor
+{
+  public:
+    NeutronHPphysics(const G4String& name = "neutronHP_Custom");
+    ~NeutronHPphysics() override;
+
+    void ConstructParticle() override {};
+    void ConstructProcess() override;
+
+  private:
+    void DefineCommands();
+
+    G4bool fThermal = true; // تفعيل التشتت الحراري تلقائياً لـ D2O و الـ BSA
+    G4GenericMessenger* fMessenger = nullptr;
+};
+
+#endif
