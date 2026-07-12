373951013f619ca9a105546f5f8a38389658457a
Maryam Abu Hishmeh <mabohishma@gmail.com>
2026-07-12 12:50:57 +0300

    Update runaction.hh

--- GEANT4/Beam_Shaping_Assembly/runaction.hh
+++ GEANT4/Beam_Shaping_Assembly/runaction.hh
@@ -23,14 +23,17 @@ class MyRunAction : public G4UserRunAction
 
   // استبدلي الـ G4int القديمة بهذا التعريف الذكي:
     G4Accumulable<G4int> nTarget = 0;
-    G4Accumulable<G4int> nNickel = 0;
+    G4Accumulable<G4int> nFastFilter = 0;
     G4Accumulable<G4int> nModerator = 0;
     G4Accumulable<G4int> nGamma = 0;
-    G4Accumulable<G4int> nThermal = 0;
     G4Accumulable<G4int> nCollimator = 0;
     G4Accumulable<G4int> nReflector = 0;
     G4Accumulable<G4int> nDetector = 0;
      
+     // أضيفي هذه الأسطر داخل تعريف الكلاس MyRunAction في ملف runaction.hh تحت العدادات الأخرى
+    G4Accumulable<G4double> dFastAccumulated = 0;
+    G4Accumulable<G4double> dGammaAccumulated = 0;
+
     G4Accumulable<G4int> nThermalFluxCount = 0; // تم تغيير الاسم هنا لمنع التكرار
     G4Accumulable<G4int> nEpithermal = 0;
     G4Accumulable<G4int> nFast = 0;
