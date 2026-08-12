d7652bb9a1f9bc211d1e862281a0229f759d3e02
Maryam Abu Hishmeh <mabohishma@gmail.com>
2026-08-12 12:17:13 +0300

    Update detector.cc

--- GEANT4/Beam_Shaping_Assembly/detector.cc
+++ GEANT4/Beam_Shaping_Assembly/detector.cc
@@ -3,6 +3,51 @@
 #include "G4RunManager.hh"
 
 MySensitiveDetector::MySensitiveDetector(G4String name) : G4VSensitiveDetector(name)
+{}
+
+MySensitiveDetector::~MySensitiveDetector()
+{}
+
+G4bool MySensitiveDetector::ProcessHits(G4Step *aStep, G4TouchableHistory *R0hist)
+{
+    // تم نقل آلية التسجيل بشكل كامل وحصري إلى SteppingAction 
+    // لمنع التكرار والحصول على ملفات CSV نظيفة ودقيقة جداً
+    return true; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+#include "detector.hh"
+#include "runaction.hh"
+#include "G4RunManager.hh"
+
+MySensitiveDetector::MySensitiveDetector(G4String name) : G4VSensitiveDetector(name)
 {} //constructor (no additional action is required => empty)
 // this constructor requires the unique name of the detector as an argument!
 
