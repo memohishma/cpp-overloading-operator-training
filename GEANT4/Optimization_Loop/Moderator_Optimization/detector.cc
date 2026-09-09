b19dc8ce6ba314031f1ac31f0b2aaf5c9220bf2b
Maryam Abu Hishmeh <mabohishma@gmail.com>
2026-09-09 13:39:02 +0300

    Create detector.cc

---
+++ GEANT4/Optimization_Loop/Moderator_Optimization/detector.cc
@@ -0,0 +1,15 @@
+#include "detector.hh"
+#include "runaction.hh"
+#include "G4RunManager.hh"
+
+MySensitiveDetector::MySensitiveDetector(G4String name) : G4VSensitiveDetector(name)
+{}
+
+MySensitiveDetector::~MySensitiveDetector()
+{}
+
+G4bool MySensitiveDetector::ProcessHits(G4Step *aStep, G4TouchableHistory *R0hist)
+{
+    return true; 
+}
+
